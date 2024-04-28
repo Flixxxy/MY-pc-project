ad7db6106870c850e39cc69c6deb15cf34084143
flixxy <almiree694@gmail.com>
2024-04-28 13:06:55 +0600

    timer

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,26 @@
-function time(){
-	var date = new Date();
-  var h = date.getHours();
-  var m = date.getMinutes();
-  var s = date.getSeconds();
-  document.getElementById("time").innerHTML = h + ":" + m + ":" + s;
-  setTimeout(time, 1000);
+const countdown = document.querySelector('.countdown')
+const targetDate = new Date('2024-04-30T00:00:00')
 
+function updateCountdown() {
+	const now = new Date()
+	const remainingTime = targetDate - now
+
+	const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24))
+	const hours = Math.floor(
+		(remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	)
+	const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60))
+	const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000)
+
+	document.getElementById('days').innerText = days.toString().padStart(2, '0')
+	document.getElementById('hours').innerText = hours.toString().padStart(2, '0')
+	document.getElementById('minutes').innerText = minutes
+		.toString()
+		.padStart(2, '0')
+	document.getElementById('seconds').innerText = seconds
+		.toString()
+		.padStart(2, '0')
 }
 
+// Обновляем счетчик каждую секунду
+setInterval(updateCountdown, 1000)
